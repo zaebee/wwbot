a64d98cacfb5abd7eb049ca549dcc5bf394bf6c6
zaebee <zaebuntu@gmail.com>
2017-10-24 17:46:36 +0300

    added slackclient

--- eventchat/views.py
+++ eventchat/views.py
@@ -1,9 +1,23 @@
+import os
 import ujson
+import logging
+
 from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
-#
+from slackclient import SlackClient
+
+
+SLACK_BOT_USER = 'wwdeploy'
+SLACK_BOT_MENTION = '<@%s>' % SLACK_BOT_USER
+SLACK_BOT_NAME = 'wwdeploy'
+SLACK_CHANNEL = 'deploy'
+
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+
+slack_client = SlackClient(SLACK_TOKEN)
+
+
 def slack_chat(request):
     data = ujson.loads(request.body)
     # import ipdb;ipdb.set_trace()
